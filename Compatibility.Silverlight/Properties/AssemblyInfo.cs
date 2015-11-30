d51c9977b9eecef4ae6b5bce420f03e16e41c628
George Birbilis <GitHub1@zoomicon.com>
2015-11-30 16:36:49 +0200

    - Added CompositeTransform (unfortunately descending from Transform needs some research, so descending from GeneralTransform which isn't very useful, since it can't be used as a RenderTransform in UIElements) - Added FocusManager class - Added KeyboardNavigation class - Updated NuGet package

--- Compatibility.Silverlight/Properties/AssemblyInfo.cs
+++ Compatibility.Silverlight/Properties/AssemblyInfo.cs
@@ -1,10 +1,19 @@
-﻿//Version: 20150206
+﻿//Project: Compatibility (http://github.com/zoomicon/Compatibility)
+//Filename: AssemblyInfo.cs
+//Version: 20151130
 
 using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Windows.Markup;
 
 [assembly: XmlnsDefinition("http://schemas.microsoft.com/winfx/2006/xaml/presentation", "System.Windows.Controls")]
+
+//for KeyboardNavigation:
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/winfx/2006/xaml/presentation", "System.Windows.Input")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/client/2007", "System.Windows.Input")]
+
+//for FocusManager:
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/client/2007", "Compatibility")]
 
 //for Label, Accordion:
 [assembly: XmlnsDefinition("http://schemas.microsoft.com/winfx/2006/xaml/presentation", "Compatibility")]
@@ -14,20 +23,20 @@ using System.Windows.Markup;
 [assembly: XmlnsDefinition("http://schemas.microsoft.com/winfx/2006/xaml/presentation/toolkit", "Compatibility")]
 [assembly: XmlnsDefinition("http://schemas.microsoft.com/winfx/2006/xaml/presentation/toolkit/extended", "Compatibility")]
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Compatibility")]
+[assembly: AssemblyTitle("Compatibility.Silverlight")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Zoomicon")]
-[assembly: AssemblyProduct("Compatibility")]
-[assembly: AssemblyCopyright("Copyright ©  2012-2015")]
+[assembly: AssemblyProduct("Compatibility.Silverlight")]
+[assembly: AssemblyCopyright("Copyright ©")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
@@ -37,11 +46,11 @@ using System.Windows.Markup;
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.*")]
 
---
+++ Source/FocusManager.cs
@@ -0,0 +1,44 @@
+﻿//Project: Compatibility (http://github.com/Zoomicon/Compatibility)
+//Filename: FocusManager.cs
+//Version: 20151130
+
+using System.Windows;
+
+namespace Compatibility
+{
+  public static class FocusManager
+  {
+
+    //See: http://referencesource.microsoft.com/#PresentationCore/Core/CSharp/System/Windows/Input/FocusManager.cs
+
+    #region IsFocusScope Property
+
+    /// <summary>
+    /// The DependencyProperty for the IsFocusScope property.
+    /// This property is used to mark the special containers (like Window, Menu) so they can
+    /// keep track of the FocusedElement element inside the container. Once focus is set
+    /// on the container - it is delegated to the FocusedElement element
+    ///     Default Value:      false
+    /// </summary>
+    public static readonly DependencyProperty IsFocusScopeProperty =
+      DependencyProperty.RegisterAttached("IsFocusScope", typeof(bool), typeof(FocusManager),
+                                          new PropertyMetadata(false
+                                            #if !SILVERLIGHT
+                                            , OnFocusScopePropertyChanged
+                                            #endif
+                                            ));
+
+    #if !SILVERLIGHT
+
+    private static void OnFocusScopePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+      UIElement element = (UIElement)d;
+      d.SetValue(System.Windows.Input.FocusManager.IsFocusScopeProperty, e.NewValue); //pass on to WPF's FocusManager.IsFocusScopeProperty implementation
+    }
+
+    #endif
+
+    #endregion
+
+  }
+}
